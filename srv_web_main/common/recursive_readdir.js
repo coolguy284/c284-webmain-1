023fb904ae6fb322df53abe0da499a90bd2aa8b5
coolguy284 <coolguy284v64@gmail.com>
2022-06-29 00:50:03 -0400

    use git repo modtimes everywhere modtimes used p2
    remove recursive_readdir caching, finish buffered move of srv_web_main/.dockerignore to root folder of repo
    
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- srv_web_main/common/recursive_readdir.js
+++ srv_web_main/common/recursive_readdir.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-function recursiveReaddirInternal(path, excludeDirs) {
+module.exports = function recursiveReaddir(path, excludeDirs) {
   try {
     if (!excludeDirs) excludeDirs = [];
     
@@ -12,31 +12,9 @@ function recursiveReaddirInternal(path, excludeDirs) {
     
     contents.forEach(x => x.isDirectory() ? folders.push(x) : files.push(x));
     
-    return [...folders.map(x => recursiveReaddirInternal(path + '/' + x.name, excludeDirs.filter(x => x.startsWith(x)).map(x => x.split('/').slice(1).join('/'))).map(y => x.name + '/' + y)).reduce((a, c) => (a.push(...c), a), []), ...files.map(x => x.name)];
+    return [...folders.map(x => recursiveReaddir(path + '/' + x.name, excludeDirs.filter(x => x.startsWith(x)).map(x => x.split('/').slice(1).join('/'))).map(y => x.name + '/' + y)).reduce((a, c) => (a.push(...c), a), []), ...files.map(x => x.name)];
   } catch (e) {
     console.error(e);
     return [];
   }
-}
-
-// keep track of past 10 unique recursive readdir calls to speed up repeated function calls
-var prevReaddirs = new Map();
-
-function recursiveReaddir(path, excludeDirs) {
-  var args = JSON.stringify([path, excludeDirs]);
-  
-  var prevCall = prevReaddirs.get(args);
-  if (prevCall)
-    return prevCall;
-  
-  var newCall = recursiveReaddirInternal(path, excludeDirs);
-  
-  prevReaddirs.set(args, newCall);
-  
-  if (prevReaddirs.size > 10)
-    prevReaddirs.delete(prevReaddirs.keys().next().value);
-  
-  return newCall;
-}
-
-module.exports = recursiveReaddirInternal;
+};
