949ac22164238c5a4f0788e0e6760bc4b42343ba
coolguy284 <coolguy284v64@gmail.com>
2022-07-07 21:17:55 -0400

    add index file for running without docker
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

---
+++ index.js
@@ -0,0 +1,28 @@
+// this file is for running bare bones version of webserver without docker
+
+var cp = require('child_process');
+var fs = require('fs');
+
+try {
+  fs.readFileSync(
+    process.platform == 'win32' ?
+      'D:/Data/c284-webmain-1_s/env.list' :
+      '/home/webmain/c284-webmain-1_s/env.list'
+  ).toString().split(/\r?\n/g).forEach(entry => {
+    if (entry[0] == '#') return;
+    let split = entry.split('=');
+    if (split.length < 2) return;
+    let key = split[0].trim();
+    let value = split.slice(1).join(':').trim();
+    process.env[key] = value;
+  });
+} catch (e) {
+  console.error('Error parsing .env');
+  console.error(e);
+}
+
+var srv_web_main = cp.spawn('node', ['index.js'], { cwd: 'srv_web_main', stdio: ['pipe', 'pipe', 'pipe'] });
+
+process.stdin.pipe(srv_web_main.stdin);
+srv_web_main.stdout.pipe(process.stdout);
+srv_web_main.stderr.pipe(process.stderr);
