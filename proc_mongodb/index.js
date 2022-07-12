3ebec59078c1774600e8c9d16bdee94aadcc7327
coolguy284 <coolguy284v64@gmail.com>
2022-07-11 20:47:43 -0400

    fix spacing in proc_mongodb index
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- proc_mongodb/index.js
+++ proc_mongodb/index.js
@@ -2,27 +2,27 @@
   var cp = require('child_process');
   var dns = require('dns');
   var net = require('net');
-
+  
   // start mongodb server and register hooks
   var proc_mongodb = cp.spawn('mongod', [
     '--dbpath', 'mongodb', '--bind_ip', '127.0.0.1', '--port', '27017', '--replSet', 'c284wm1_1'
   ], { stdio: ['pipe', 'pipe', 'pipe'] });
-
+  
   process.stdin.pipe(proc_mongodb.stdin);
   proc_mongodb.stdout.pipe(process.stdout);
   proc_mongodb.stderr.pipe(process.stderr);
-
+  
   process.on('SIGINT', () => {
     proc_mongodb.kill('SIGINT');
   });
-
+  
   proc_mongodb.on('exit', () => {
     server.close();
     for (var conn of serverConns) conn.destroy();
     setTimeout(() => process.exit(), 5000).unref();
   });
-
-
+  
+  
   // start reverse proxy
   var srv_web_main_addrs = new Set((await dns.promises.lookup('srv_web_main', { all: true })).map(x => x.family == 4 ? '::ffff:' + x.address : x.address));
   var serverConns = new Set();
@@ -52,7 +52,7 @@
   server.on('error', err => {
     console.log(`{"t":{"$date":"${new Date().toISOString().slice(0, -1)}+00:00"},"ctx":"c284-node","c":"MAIN","msg":"Reverse Proxy ${err.toString()}"}`);
   });
-
+  
   server.listen(27016, () => {
     console.log(`{"t":{"$date":"${new Date().toISOString().slice(0, -1)}+00:00"},"ctx":"c284-node","c":"MAIN","msg":"Mongodb reverse proxy server listening"}`);
   });
