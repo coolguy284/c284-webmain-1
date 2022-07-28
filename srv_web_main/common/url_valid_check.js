ac225ff36c4ff97bc85fe4a2824c20a2d2b24ff1
coolguy284 <coolguy284v64@gmail.com>
2022-07-28 17:49:36 -0400

    add caching to url valid check
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- srv_web_main/common/url_valid_check.js
+++ srv_web_main/common/url_valid_check.js
@@ -21,7 +21,9 @@ var common = {
 
 var _statErrorCodes = new Set(['ENOENT', 'ENOTDIR', 'EISDIR', 'ERR_INVALID_ARG_VALUE']);
 
-module.exports = exports = async function isValidUrl(url) {
+var doCaching = true;
+
+async function isValidUrl(url) {
   var publicPath = common.getPublicPath(url);
   
   if (!common.isSubDir('websites/public', publicPath))
@@ -31,6 +33,8 @@ module.exports = exports = async function isValidUrl(url) {
     return false;
   
   let match;
+  
+  if (url.startsWith('/old/') || url.startsWith('/old2/') || url.startsWith('/oldg/')) return true;
   
   if (url.startsWith('/misc/unicode/') && (match = /^\/misc\/unicode\/([Uu])\+((?:|0?[0-9A-Fa-f]|10)[0-9A-Fa-f]{4})$/.exec(url))) {
     let fancyCodePoint = parseInt(match[2], 16).toString(16).toUpperCase().padStart(4, '0');
@@ -53,6 +57,47 @@ module.exports = exports = async function isValidUrl(url) {
     else
       throw err;
   }
+}
+
+var urlCache = new Map();
+var urlCurrentAwaits = new Map();
+
+module.exports = exports = async function isValidUrlCache(url) {
+  if (!doCaching) return await isValidUrl(url);
+  
+  let cachedResponse = urlCache.get(url);
+  if (cachedResponse != null) return cachedResponse;
+  
+  let currentAwait = urlCurrentAwaits.get(url);
+  if (currentAwait)
+    return await new Promise(r => {
+      currentAwait.push(r);
+    });
+  
+  let responseAwait = isValidUrl(url);
+  
+  currentAwait = [];
+  urlCurrentAwaits.set(url, currentAwait);
+  
+  let response = await responseAwait;
+  
+  urlCache.set(url, response);
+  
+  new Promise(r => {
+    currentAwait.forEach(x => x(response));
+    urlCurrentAwaits.delete(url);
+    r();
+  });
+  
+  return response;
 };
 
 exports._statErrorCodes = _statErrorCodes;
+exports.isValidUrl = isValidUrl;
+exports.urlCache = urlCache;
+Object.defineProperty(exports, 'doCaching', {
+  configurable: true,
+  enumerable: true,
+  get: () => doCaching,
+  set: v => { doCaching = v; },
+});
