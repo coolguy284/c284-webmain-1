25f7e0cec3187c05d4980395bf652850fb910cbe
coolguy284 <coolguy284v64@gmail.com>
2022-07-27 22:19:58 -0400

    improve handling of redirs in redir file
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- srv_web_main/common/redirects.js
+++ srv_web_main/common/redirects.js
@@ -1,95 +1,125 @@
 var fs = require('fs');
-
 var parseArgString = require('./arg_string_parse');
 
-var serverTypeMapping = { 'F': 0, 'D': 1, 'R': 2 };
-var sendTypeMapping = { 'TS': 307, 'TG': 303, 'T-': 302, 'PS': 308, 'P-': 301 };
+var serverTypeMapping = { 'F': 'file', 'D': 'directory', 'R': 'regex', 'C': 'custom' };
+var sendStatusCodeMapping = { 'TS': 307, 'TG': 303, 'T-': 302, 'PS': 308, 'P-': 301 };
 
-var fileRedirects = [], folderRedirects = [], regexRedirects = [];
+var redirects = [];
 
 // parse redirect array
 fs.readFileSync('websites/redirects.txt').toString()
   .split('\n')
   .filter(entry => entry && !entry.startsWith('#'))
   .map(parseArgString)
-  .filter(x => x.length >= 3)
+  .filter(x => x.length >= 2)
   .forEach(x => {
-    let serverTypeStr = x[0][0], sendTypeStr = x[0].slice(1);
+    let serverTypeStr = x[0][0], sendStatusCodeStr = x[0].slice(1);
     
     if (!(serverTypeStr in serverTypeMapping))
       throw new Error(`redirect serverType ${serverType} is invalid`);
     
-    if (!(sendTypeStr in sendTypeMapping))
-      throw new Error(`redirect sendType ${sendType} is invalid`);
+    if (!(sendStatusCodeStr in sendStatusCodeMapping))
+      throw new Error(`redirect sendStatusCode ${sendStatusCode} is invalid`);
     
-    let serverType = serverTypeMapping[serverTypeStr], sendType = sendTypeMapping[sendTypeStr];
+    let serverType = serverTypeMapping[serverTypeStr], sendStatusCode = sendStatusCodeMapping[sendStatusCodeStr];
     
     switch (serverType) {
-      case 0: fileRedirects.push([x[1], [x[2], sendType]]); break;
-      case 1: folderRedirects.push([x[1], x[2], sendType]); break;
-      case 2: regexRedirects.push([new RegExp(x[1]), x[2], sendType]); break;
+      case 'file': {
+        if (x.length < 3) return;
+        let prevRedirect = redirects[redirects.length - 1];
+        if (prevRedirect?.type == 'file')
+          prevRedirect.redirects.push([x[1], { to: x[2], statusCode: sendStatusCode }]);
+        else
+          redirects.push({ type: serverType, redirects: [[x[1], { to: x[2], statusCode: sendStatusCode } ]] });
+        break;
+      }
+      case 'directory':
+        if (x.length < 3) return;
+        redirects.push({ type: serverType, from: x[1], to: x[2], statusCode: sendStatusCode });
+        break;
+      case 'regex':
+        if (x.length < 3) return;
+        redirects.push({ type: serverType, from: new RegExp(x[1]), to: x[2], statusCode: sendStatusCode });
+        break;
+      case 'custom':
+        if (x.length < 2) return;
+        redirects.push({ type: serverType, name: x[1] });
+        break;
     }
   });
 
-fileRedirects = Object.fromEntries(fileRedirects);
+for (var redirect of redirects) {
+  if (redirect.type == 'file') redirect.redirects = Object.fromEntries(redirect.redirects);
+}
 
 // takes in a url object and returns [false] if no redirects, else returns [true, statusCode, locationString]
 function followRedirects(url) {
   let urlString = url.pathname;
   
-  let didRedirect = false, redirectStatus;
+  let didRedirect = false, statusCode;
   
-  // regexs first
-  for (var regexRedirect of regexRedirects) {
-    let replacedURL = urlString.replace(regexRedirect[0], regexRedirect[1]);
-    if (urlString != replacedURL) {
-      didRedirect = true;
-      redirectStatus = regexRedirect[2];
-      urlString = replacedURL;
-    }
-  }
-  
-  // folders next
-  for (var folderRedirect of folderRedirects) {
-    if (urlString.startsWith(folderRedirect[0])) {
-      didRedirect = true;
-      redirectStatus = folderRedirect[2];
-      urlString = folderRedirect[1] + urlString.slice(folderRedirect[0].length);
-    }
-  }
-  
-  // files last
-  if (urlString in fileRedirects) {
-    let fileRedirect = fileRedirects[urlString];
-    didRedirect = true;
-    redirectStatus = fileRedirect[1];
-    urlString = fileRedirect[0];
-  }
-  
-  if (urlString == '/misc/unicode/U+0NAN') {
-    didRedirect = true;
-    redirectStatus = 308;
-    urlString = '/misc/unicode/U+';
-  }
-  
-  let match;
-  if (urlString.startsWith('/misc/unicode/') && (match = /^\/misc\/unicode\/(?:([Uu])\+(0{0,2}(?:0?[0-9A-Fa-f]|10)?[0-9A-Fa-f]{0,4}))$/.exec(urlString))) {
-    let fancyCodePoint = match[2] ? parseInt(match[2], 16).toString(16).toUpperCase().padStart(4, '0') : '0000';
-    if (match[1] == 'u' || fancyCodePoint != match[2]) {
-      didRedirect = true;
-      redirectStatus = 308;
-      urlString = '/misc/unicode/U+' + fancyCodePoint;
+  // process redirects one at a time in same order as redirects file, except every file redirect in a row is processed at once
+  for (var redirect of redirects) {
+    switch (redirect.type) {
+      case 'file': {
+        let fileRedirect = redirect.redirects[urlString];
+        if (fileRedirect != null) {
+          didRedirect = true;
+          statusCode = fileRedirect.statusCode;
+          urlString = fileRedirect.to;
+        }
+        break;
+      }
+      case 'directory': {
+        if (urlString.startsWith(redirect.from)) {
+          didRedirect = true;
+          statusCode = redirect.statusCode;
+          urlString = redirect.to + urlString.slice(redirect.from.length);
+        }
+        break;
+      }
+      case 'regex': {
+        let replacedURL = urlString.replace(redirect.from, redirect.to);
+        if (urlString != replacedURL) {
+          didRedirect = true;
+          statusCode = redirect.statusCode;
+          urlString = replacedURL;
+        }
+        break;
+      }
+      case 'custom':
+        switch (redirect.name) {
+          case 'unicode': {
+            if (urlString == '/misc/unicode/U+0NAN') {
+              didRedirect = true;
+              statusCode = redirect.statusCode;
+              urlString = '/misc/unicode/U+';
+            }
+            
+            let match;
+            if (urlString.startsWith('/misc/unicode/') && (match = /^\/misc\/unicode\/(?:([Uu])\+(0{0,2}(?:0?[0-9A-Fa-f]|10)?[0-9A-Fa-f]{0,4}))$/.exec(urlString))) {
+              let fancyCodePoint = match[2] ? parseInt(match[2], 16).toString(16).toUpperCase().padStart(4, '0') : '0000';
+              if (match[1] == 'u' || fancyCodePoint != match[2]) {
+                didRedirect = true;
+                statusCode = redirect.statusCode;
+                urlString = '/misc/unicode/U+' + fancyCodePoint;
+              }
+            }
+            break;
+          }
+        }
+        break;
     }
   }
   
   // return appropriate url
   if (didRedirect) {
     if (urlString.startsWith('http://') || urlString.startsWith('https://')) {
-      return [true, redirectStatus, urlString];
+      return [true, statusCode, urlString];
     } else {
       let newURL = new URL(url);
       newURL.pathname = urlString;
-      return [true, redirectStatus, newURL.href];
+      return [true, statusCode, newURL.href];
     }
   } else {
     return [false];
@@ -98,9 +128,7 @@ function followRedirects(url) {
 
 module.exports = exports = {
   followRedirects,
-  fileRedirects,
-  folderRedirects,
-  regexRedirects,
+  redirects,
   serverTypeMapping,
-  sendTypeMapping,
+  sendStatusCodeMapping,
 };
