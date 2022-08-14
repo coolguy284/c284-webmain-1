f65ce2007598e1ef756a84eaf25a603d44f72e8d
coolguy284 <coolguy284v64@gmail.com>
2022-08-13 22:39:33 -0400

    update docker debug mode scripts to match
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- srv_web_main/docker/start_debug.sh
+++ srv_web_main/docker/start_debug.sh
@@ -1,8 +1,8 @@
 # this file is a reference only, service is started from proc_main
 
 sudo docker run --rm -i --name c284-webmain-1_srv_web_main --network ${NETWORK_NAME} --network-alias srv_web_main \
-  --env-file /home/webmain/c284-webmain-1_s/env.list \
   --mount type=bind,source=/home/webmain/c284-webmain-1_s/cert,target=/home/webmain/cert,readonly \
   --mount type=bind,source=/home/webmain/c284-webmain-1/srv_web_main/websites,target=/home/webmain/websites,readonly \
+  --env-file /home/webmain/c284-webmain-1_s/env.list \
   -p 80:8080 -p 443:8443 \
   c284-webmain-1_srv_web_main
