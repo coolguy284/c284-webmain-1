1682e42429177f4ceec91c068f0517078ded1a11
coolguy284 <coolguy284v64@gmail.com>
2023-06-22 00:04:00 -0400

    update packages and node.js ver
    update path of bson copying to websites/public/libs/extern, update filename of bson in websites/public/libs/extern as a result
    
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- srv_web_main/docker/build_no_cache.sh
+++ srv_web_main/docker/build_no_cache.sh
@@ -1,3 +1,3 @@
 cd "${0%/*}/.."
 
-sudo docker build --build-arg NODE_VERSION=19.3.0 --no-cache -t coolguy284/c284-webmain-1_srv_web_main -f Dockerfile ..
+sudo docker build --build-arg NODE_VERSION=20.3.1 --no-cache -t coolguy284/c284-webmain-1_srv_web_main -f Dockerfile ..
--- srv_web_old2/docker/build_no_cache.sh
+++ srv_web_old2/docker/build_no_cache.sh
@@ -1,1 +1,1 @@
-sudo docker build --build-arg NODE_VERSION=19.3.0 --no-cache -t coolguy284/c284-webmain-1_srv_web_old2 "${0%/*}/.."
+sudo docker build --build-arg NODE_VERSION=20.3.1 --no-cache -t coolguy284/c284-webmain-1_srv_web_old2 "${0%/*}/.."
