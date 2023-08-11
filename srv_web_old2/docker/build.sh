a9f5675fcba3bee696cf24c26b92f4341a7a609a
coolguy284 <coolguy284v64@gmail.com>
2023-08-11 02:07:03 -0400

    update node.js version
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- srv_web_old2/docker/build.sh
+++ srv_web_old2/docker/build.sh
@@ -1,1 +1,1 @@
-sudo docker build --build-arg NODE_VERSION=20.5.0 -t coolguy284/c284-webmain-1_srv_web_old2 "${0%/*}/.."
+sudo docker build --build-arg NODE_VERSION=20.5.1 -t coolguy284/c284-webmain-1_srv_web_old2 "${0%/*}/.."
