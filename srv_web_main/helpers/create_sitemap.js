007c8776bf48b6d5f338483f79fea223ea72139a
coolguy284 <coolguy284v64@gmail.com>
2024-02-16 12:23:11 -0500

    improve sitemap generation logic
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- srv_web_main/helpers/create_sitemap.js
+++ srv_web_main/helpers/create_sitemap.js
@@ -16,13 +16,38 @@ async function createSitemap(sites, siteModTimesObj) {
     '<urlset xmlns = \'https://www.sitemaps.org/schemas/sitemap/0.9\'>\n' +
     (await Promise.all(
       Array.from(sites.entries()).map(async site => {
-        let url = `https://coolguy284.com${site[0].endsWith('index.html') ? site[0].slice(0, -10) : site[0]}`;
-        let escapedUrl = url.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;').replace(/\r/g,'&#13;');
+        let rawURL = `https://coolguy284.com${site[0].endsWith('index.html') ? site[0].slice(0, -10) : site[0]}`;
+        let escapedURL = rawURL.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;').replace(/\r/g,'&#13;');
+        
+        let lastModDateObj = siteModTimesObj ? siteModTimesObj[site[0].slice(1)] : (await fs.promises.stat('websites/public' + site[0])).mtime;
+        let lastModDate = lastModDateObj.toISOString().slice(0, -1);
+        
+        let changeFreq;
+        if (websiteData[site[0].slice(1)] & 1) {
+          // file is library file
+          changeFreq = 'yearly';
+        } else {
+          // file is normal file
+          let daysAgo = (Date.now() - lastModDateObj.getTime()) / 1000 / 3600 / 24;
+          
+          if (daysAgo < 3) {
+            changeFreq = 'daily';
+          } else if (daysAgo < 4 * 7) {
+            changeFreq = 'weekly';
+          } else if (daysAgo < 3 * 30) {
+            changeFreq = 'monthly';
+          } else {
+            changeFreq = 'yearly';
+          }
+        }
+        
+        let priority = (site[0].startsWith('/debug') || site[0].startsWith('/errors') ? 0.0 : 1.0 - site[1] * 0.25).toFixed(1);
+        
         return '  <url>\n' +
-          `    <loc>${escapedUrl}</loc>\n` +
-          `    <lastmod>${(siteModTimesObj ? siteModTimesObj[site[0].slice(1)] : (await fs.promises.stat('websites/public' + site[0])).mtime).toISOString().slice(0, -1)}+00:00</lastmod>\n` +
-          `    <changefreq>${websiteData[site[0].slice(1)] & 1 ? 'yearly' : 'daily'}</changefreq>\n` +
-          `    <priority>${(site[0].startsWith('/debug') || site[0].startsWith('/errors') ? 0.0 : 1.0 - site[1] / 10).toFixed(1)}</priority>\n` +
+          `    <loc>${escapedURL}</loc>\n` +
+          `    <lastmod>${lastModDate}+00:00</lastmod>\n` +
+          `    <changefreq>${changeFreq}</changefreq>\n` +
+          `    <priority>${priority}</priority>\n` +
           '  </url>';
       })
     )).join('\n  \n') + '\n' +
