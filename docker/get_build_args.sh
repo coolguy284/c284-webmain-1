17f3bc31ca76b7e4ab69b8feff059d45b55faecb
coolguy284 <coolguy284v64@gmail.com>
2024-03-14 03:14:39 -0400

    update packages and node ver
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- docker/get_build_args.sh
+++ docker/get_build_args.sh
@@ -7,5 +7,5 @@ if [ "$path_name" = "srv_web_oldg" ]; then
   echo -n ' --build-arg ZLIB_VER=1.3.1'
 else
   echo -n ' --build-arg NODE_MAJOR_VER=21'
-  echo -n ' --build-arg NODE_FULL_VER=21.6.2'
+  echo -n ' --build-arg NODE_FULL_VER=21.7.1'
 fi
