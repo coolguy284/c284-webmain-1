8b38c7d77b2bf96c5198c06196a0af6150329933
coolguy284 <coolguy284v64@gmail.com>
2024-06-03 14:12:38 -0400

    add basic functionality to calculator
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- srv_web_main/websites/public/apps/calculator/script.js
+++ srv_web_main/websites/public/apps/calculator/script.js
@@ -1,3 +1,14 @@
+let MAX_COMMAND_HISTORY = 1000;
+let MAX_RESULT_HISTORY = 2000;
+
+let commandHistory = [];
+let resultHistory = [];
+let mathCtx = math.create({
+  number: 'BigNumber',
+  precision: 64,
+});
+let calcParser = mathCtx.parser();
+
 function toggleElem(elem) {
   if (elem.style.display == 'none') {
     elem.style.display = '';
@@ -14,8 +25,45 @@ function toggleHelp() {
   toggleElem(help_div);
 }
 
+function mathObjToString(obj) {
+  if (obj === undefined) {
+    return 'undefined';
+  } else if (obj === null) {
+    return 'null';
+  } else {
+    return obj.toString();
+  }
+}
+
 function calculate(string) {
-  return string;
+  try {
+    let result = calcParser.evaluate(string);
+    calcParser.set('ans', result);
+    return mathObjToString(result);
+  } catch (err) {
+    return err.toString();
+  }
+}
+
+function getVariables() {
+  return Array.from(calcParser.getAllAsMap()).map(([name, value]) => `${name} = ${mathObjToString(value)}`);
+}
+
+function updateResultHistory() {
+  calculator_output.innerText = resultHistory.join('\n');
+}
+
+function appendUpdateResultHistory(number) {
+  updateResultHistory();
+}
+
+function updateVariablesList() {
+  let vars = getVariables();
+  if (vars.length == 0) {
+    vars_list.innerHTML = '<em>Empty</em>';
+  } else {
+    vars_list.innerText = vars.join('\n');
+  }
 }
 
 setting_mathjs_precision.addEventListener('change', evt => {
@@ -26,7 +74,21 @@ setting_mathjs_precision.addEventListener('change', evt => {
 
 calculator_input.addEventListener('keypress', evt => {
   if (evt.code == 'Enter') {
-    calculate(calculator_input.value);
+    let commandStr = calculator_input.value;
+    let resultStr = calculate(commandStr);
     calculator_input.value = '';
+    commandHistory.push(commandStr);
+    resultHistory.push(`-> ${commandStr}`);
+    resultHistory.push(`<- ${resultStr}`);
+    appendUpdateResultHistory(2);
+    updateVariablesList();
   }
 });
+
+function init() {
+  updateResultHistory();
+  updateVariablesList();
+  calculator_input.focus();
+}
+
+init();
