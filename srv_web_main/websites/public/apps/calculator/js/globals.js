fcd6ed61a99390a2c5311c61a953f323317d9fee
coolguy284 <coolguy284v64@gmail.com>
2024-06-06 08:52:20 -0400

    calculator: add object & clearing funcs
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- srv_web_main/websites/public/apps/calculator/js/globals.js
+++ srv_web_main/websites/public/apps/calculator/js/globals.js
@@ -23,3 +23,71 @@ let mathCtx = math.create({
   precision: state.cfg.precision,
 });
 let calcParser = mathCtx.parser();
+
+mathCtx.import({
+  has: (obj, prop) => {
+    if (prop == null) {
+      prop = obj;
+      return calcParser.scope.has(prop);
+    } else {
+      return prop in obj;
+    }
+  },
+  get: (obj, prop) => {
+    if (prop == null) {
+      prop = obj;
+      return calcParser.get(prop);
+    } else {
+      return obj[prop];
+    }
+  },
+  set: (obj, prop, value) => {
+    if (prop == null) {
+      value = prop;
+      prop = obj;
+      return calcParser.set(prop, value);
+    } else {
+      Object.defineProperty(obj, prop, {
+        value,
+        writable: true,
+        configurable: true,
+        enumerable: true,
+      });
+    }
+  },
+  remove: (obj, prop) => {
+    if (prop == null) {
+      prop = obj;
+      return calcParser.remove(prop);
+    } else {
+      delete obj[prop];
+    }
+  },
+  getKeys: obj => {
+    if (obj == null) {
+      return Array.from(calcParser.getAllAsMap().keys());
+    } else {
+      return Object.keys(obj);
+    }
+  },
+  clear: obj => {
+    if (obj == null) {
+      calcParser.clear();
+    } else {
+      for (let key in obj) {
+        delete obj[key];
+      }
+    }
+  },
+  
+  clearHistory: () => {
+    state.commandHistory.splice(0);
+    state.resultHistory.splice(0);
+    updateResultHistory();
+  },
+  
+  clearAll: () => {
+    mathCtx.clear();
+    mathCtx.clearHistory();
+  },
+});
