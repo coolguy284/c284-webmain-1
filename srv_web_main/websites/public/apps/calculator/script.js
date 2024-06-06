5f53bae17cff096f723d726b1573078b9eeb81e4
coolguy284 <coolguy284v64@gmail.com>
2024-06-06 01:09:30 -0400

    calculator: fix precision syncing & validation
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- srv_web_main/websites/public/apps/calculator/script.js
+++ srv_web_main/websites/public/apps/calculator/script.js
@@ -185,6 +185,8 @@ function init() {
   loadFromLocalStorage();
   updateResultHistory();
   updateVariablesList();
+  setting_mathjs_precision.value = state.cfg.precision;
+  mathCtx.config({ precision: state.cfg.precision });
   calculator_input.focus();
 }
 
