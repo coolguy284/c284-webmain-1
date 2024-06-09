dae28508a199531fe01e852d5614deac66c2a460
coolguy284 <coolguy284v64@gmail.com>
2024-06-08 22:32:37 -0400

    calculator: add checkboxes to configure persistence
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- srv_web_main/websites/public/apps/calculator/js/storage.js
+++ srv_web_main/websites/public/apps/calculator/js/storage.js
@@ -5,14 +5,24 @@ function loadFromLocalStorage() {
   state.resultHistory = stateFromSave.resultHistory ?? [];
   state.cfg = {};
   state.cfg.precision = stateFromSave.cfg?.precision ?? DEFAULT_PRECISION;
+  setting_save_history.checked = stateFromSave.cfg?.saveHistory ?? DEFAULT_SAVE_HISTORY;
+  setting_save_results.checked = stateFromSave.cfg?.saveResults ?? DEFAULT_SAVE_RESULTS;
+  setting_save_variables.checked = stateFromSave.cfg?.saveVariables ?? DEFAULT_SAVE_VARIABLES;
   stateSetVars(stateFromSave.vars ?? []);
   setViewMode(stateFromSave.ui?.viewMode ?? DEFAULT_VIEW_MODE);
 }
 
 function saveToLocalStorage() {
   let stateToSave = {
-    ...state,
-    vars: stateGetVars(),
+    commandHistory: setting_save_history.checked ? state.commandHistory : null,
+    resultHistory: setting_save_results.checked ? state.resultHistory : null,
+    cfg: {
+      ...state.cfg,
+      saveHistory: setting_save_history.checked,
+      saveResults: setting_save_results.checked,
+      saveVariables: setting_save_variables.checked,
+    },
+    vars: setting_save_variables.checked ? stateGetVars() : null,
     ui: {
       viewMode: getViewMode(),
     },
