31650f11f1cbb9573942f045a7e8724b0cfdf139
coolguy284 <coolguy284v64@gmail.com>
2024-09-06 00:34:23 -0400

    implement code to get service worker status
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- srv_web_main/websites/public/service_worker.js
+++ srv_web_main/websites/public/service_worker.js
@@ -1,1 +1,23 @@
 let currentServiceWorkerHash = '{currentServiceWorkerHash}';
+
+addEventListener('message', evt => {
+  let data = evt.data;
+  
+  if (typeof data != 'object') {
+    console.error(`Invalid message: ${data}`);
+  } else {
+    switch (data.type) {
+      case 'getStatus':
+        evt.source.postMessage({
+          type: 'status',
+          data: {
+            currentServiceWorkerHash,
+          },
+        });
+        break;
+      
+      default:
+        console.error(`Invalid message type: ${data.type}`);
+    }
+  }
+});
