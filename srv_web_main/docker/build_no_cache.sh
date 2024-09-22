e65f9d620fe59cb2b9cd4c2a9559742dcae41a38
coolguy284 <coolguy284v64@gmail.com>
2024-09-22 18:19:35 -0400

    remove invalid target directive breaking prod
    details: remove invalid target directive breaking production
    
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- srv_web_main/docker/build_no_cache.sh
+++ srv_web_main/docker/build_no_cache.sh
@@ -1,3 +1,3 @@
 cd "${0%/*}/.."
 
-sudo docker build $(../docker/get_build_args.sh) --build-arg branch=production --target production --no-cache -t coolguy284/c284-webmain-1_srv_web_main -f Dockerfile ..
+sudo docker build $(../docker/get_build_args.sh) --build-arg branch=production --no-cache -t coolguy284/c284-webmain-1_srv_web_main -f Dockerfile ..
