3b6758b05b46614c87b0906d3d984b3fd4fdeaab
coolguy284 <coolguy284v64@gmail.com>
2025-06-21 02:16:46 -0400

    update packages, software ver

--- docker/get_build_args.sh
+++ docker/get_build_args.sh
@@ -2,10 +2,10 @@
 # https://stackoverflow.com/questions/10654135/take-the-last-part-of-the-folder-path-in-shell/10664847#10664847
 path_name="${PWD##*/}"
 
-echo -n ' --build-arg NODE_MAJOR_VER=23'
-echo -n ' --build-arg NODE_FULL_VER=23.1.0'
+echo -n ' --build-arg NODE_MAJOR_VER=24'
+echo -n ' --build-arg NODE_FULL_VER=24.2.0'
 
 if [ "$path_name" = "srv_web_oldg" ]; then
-  echo -n ' --build-arg NGINX_VER=1.27.2'
+  echo -n ' --build-arg NGINX_VER=1.28.0'
   echo -n ' --build-arg ZLIB_VER=1.3.1'
 fi
