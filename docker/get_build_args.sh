57d152457af9746fce660eb5b05d49b02bfa2014
coolguy284 <coolguy284v64@gmail.com>
2025-11-23 23:40:23 -0500

    update packages

--- docker/get_build_args.sh
+++ docker/get_build_args.sh
@@ -3,9 +3,9 @@
 path_name="${PWD##*/}"
 
 echo -n ' --build-arg NODE_MAJOR_VER=25'
-echo -n ' --build-arg NODE_FULL_VER=25.0.0'
+echo -n ' --build-arg NODE_FULL_VER=25.2.1'
 
 if [ "$path_name" = "srv_web_oldg" ]; then
-  echo -n ' --build-arg NGINX_VER=1.29.2'
+  echo -n ' --build-arg NGINX_VER=1.29.3'
   echo -n ' --build-arg ZLIB_VER=1.3.1'
 fi
